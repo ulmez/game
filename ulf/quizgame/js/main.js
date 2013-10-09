3afcd28edd85e1fd9db8b58e275ccbafa06846bd
ulme <ulmezz@gmail.com>
2013-10-09 20:05:57 +0200

    new stuff

--- ulf/quizgame/js/main.js
+++ ulf/quizgame/js/main.js
@@ -1,5 +1,5 @@
 /*jslint browser:true */
-/*global alert: false, confirm: false, console: false, Debug: false, opera: false, prompt: false, WSH: false */
+/*global $: false, alert: false, confirm: false, console: false, Debug: false, opera: false, prompt: false, WSH: false */
 var arrQuestions = [];
 
 function QuestionObject(question, answers, correctAnswer) {
@@ -9,9 +9,66 @@ function QuestionObject(question, answers, correctAnswer) {
     this.correctAnswer = correctAnswer;
 }
 
+function getQuestionText(num) {
+    "use strict";
+    return arrQuestions[num].question;
+}
+
+function getQuestionAnswers(num) {
+    "use strict";
+    return arrQuestions[num].answers;
+}
+
+function getCorrectAnswer(num) {
+    "use strict";
+    return arrQuestions[num].correctAnswer;
+}
+
+//UI delen------------------------------
+
+function setAnswers(num) {
+    "use strict";
+    var i;
+
+    $("#question").text(getQuestionText(num));
+
+    $("span").each(function (index) {
+        $("#answer" + index).remove();
+    });
+
+    for (i = 0; i < getQuestionAnswers(num).length; i = i + 1) {
+        $("#placeholderAnswers").append('<span id="answer' + i + '"></span>');
+        $("#answer" + i).text(getQuestionAnswers(num)[i] + " ");
+    }
+
+    $("span").each(function (index) {
+        $("#answer" + index).click(function () {
+            alert($(this).text());
+        });
+    });
+
+    $("#correctAnswer").text(getCorrectAnswer(num));
+}
+
+function deleteAnswers() {
+    "use strict";
+    $("span").each(function (index) {
+        $("#answer" + index).remove();
+    });
+}
+//--------------------------------------
+
 arrQuestions.push(new QuestionObject("what name?", ["flum", "dummo", "frittjof"], "dummo"));
-arrQuestions.push(new QuestionObject("How are you?", ["good", "bad", "both"], "bad"));
+arrQuestions.push(new QuestionObject("How are you?", ["good", "bad", "both", "neither"], "bad"));
 arrQuestions.push(new QuestionObject("What color do I like?", ["red", "green", "yellow"], "yellow"));
 arrQuestions.push(new QuestionObject("What city?", ["Stockholm", "London", "New York"], "Stockholm"));
 
-console.log(arrQuestions);
+setAnswers(0);
+//setAnswers(1);
+//setAnswers(2);
+//setAnswers(3);
+
+//console.log(arrQuestions);
+//console.log(getQuestionText(0));
+//console.log(getQuestionAnswers(0));
+//console.log(getCorrectAnswer(0));
